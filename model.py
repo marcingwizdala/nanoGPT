948d825408b82819b893a353089471e445516a81
marcingwizdala <marcin.g@tidio.net>
2024-07-14 15:08:16 +0200

    smaller model

--- model.py
+++ model.py
@@ -214,6 +214,7 @@ class GPT(nn.Module):
 
         # n_layer, n_head and n_embd are determined from model_type
         config_args = {
+            'gpt2-small':   dict(n_layer=8, n_head=8, n_embd=512),  # 124M params
             'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
             'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
